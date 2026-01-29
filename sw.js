10f64a64cbe973a8f9efaa4784e19c0e1f5b83e7
Ranjit Patil <71892385+ranjit485@users.noreply.github.com>
2026-01-30 00:23:17 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// sw.js - Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('fleet-v1').then((cache) => cache.addAll(['/'])));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+});
